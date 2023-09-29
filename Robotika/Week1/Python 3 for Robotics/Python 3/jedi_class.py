907803d24378408d30daddb85aff955a5a5d2f53
alvansoleh <134778608+alvansoleh@users.noreply.github.com>
2023-09-29 15:28:28 +0700

    Add files via upload

---
+++ Robotika/Week1/Python 3 for Robotics/Python 3/jedi_class.py
@@ -0,0 +1,12 @@
+class Jedi:
+    def __init__(self, name):
+        self.jedi_name = name
+
+    def say_hi(self):
+        print('Hello, my name is ', self.jedi_name)
+
+j1 = Jedi('ObiWan')
+j1.say_hi()
+
+j2 = Jedi('Anakin')
+j2.say_hi()
